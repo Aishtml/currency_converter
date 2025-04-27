4014b2f3aff0fa9113695949ee88ccabb4217180
Aisdev <aisosamatthew247@gmail.com>
2025-04-27 10:36:55 +0000

    convert from dollar to naira

--- lib/main.dart
+++ lib/main.dart
@@ -1,41 +1,77 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-  // This widget is the root of your application.
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      // Application name
-      title: 'Flutter Hello World',
-      // Application theme data, you can set the colors for the application as
-      // you want
-      theme: ThemeData(
-        // useMaterial3: false,
-        primarySwatch: Colors.blue,
-      ),
-      // A widget which will be started on application startup
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
+void main() {
+  runApp(const ConverterApp());
 }
 
-class MyHomePage extends StatelessWidget {
-  final String title;
-  const MyHomePage({super.key, required this.title});  
+class ConverterApp extends StatefulWidget {
+  const ConverterApp({super.key});
+
+  @override
+  State<ConverterApp> createState() => _ConverterAppState();
+}
+
+class _ConverterAppState extends State<ConverterApp> {
+  double result = 0;
+  final TextEditingController textEditingController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        // The title text which will be shown on the action bar
-        title: Text(title),
+    const borders = OutlineInputBorder(
+      borderRadius: BorderRadius.all(Radius.circular(10)),
+      borderSide: BorderSide(
+        color: Colors.black,
       ),
-      body: Center(
-        child: Text(
-          'Hello, World!',
+    );
+
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Currency Converter'),
+          backgroundColor: Colors.black,
+          foregroundColor: Colors.white,
+        ),
+        backgroundColor: Colors.blueGrey,
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Naira $result',
+                style: const TextStyle(
+                    fontSize: 42,
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white),
+              ),
+              Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: TextField(
+                  controller: textEditingController,
+                  style: const TextStyle(
+                    color: Colors.black,
+                  ),
+                  decoration: const InputDecoration(
+                      focusedBorder: borders,
+                      enabledBorder: borders,
+                      prefixIcon: Icon(Icons.monetization_on_outlined),
+                      filled: true,
+                      fillColor: Colors.white,
+                      hintText: 'Enter your Naira'),
+                ),
+              ),
+              TextButton(
+                  onPressed: () {
+                    result = double.parse(textEditingController.text) * 1608.72;
+                    setState(() {});
+                  },
+                  style: const ButtonStyle(
+                      backgroundColor: WidgetStatePropertyAll(Colors.black),
+                      foregroundColor: WidgetStatePropertyAll(Colors.white),
+                      fixedSize: WidgetStatePropertyAll(Size(345, 50))),
+                  child: const Text('CONVERT'))
+            ],
+          ),
         ),
       ),
     );
